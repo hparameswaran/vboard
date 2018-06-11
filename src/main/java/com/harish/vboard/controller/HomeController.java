46c2f76578171154c044a63183182cda461acf4b
hparameswaran <harishp@hotmail.com>
2018-06-10 20:48:05 -0400

    first commit

---
+++ src/main/java/com/harish/vboard/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.harish.vboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value = "/")
+	public String showHome() {
+		return "home";
+	}
+}
