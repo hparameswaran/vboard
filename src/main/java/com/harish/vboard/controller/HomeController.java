20bc8fd40ece4c23bfa414d0d8820f4cd2bca526
hparameswaran <harishp@hotmail.com>
2018-06-23 15:32:46 -0400

    almost there

--- src/main/java/com/harish/vboard/controller/HomeController.java
+++ src/main/java/com/harish/vboard/controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.harish.vboard.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+	
 	@RequestMapping(value = "/")
 	public String showHome() {
+		LOGGER.debug("Rendering home page");
 		return "home";
 	}
 }
